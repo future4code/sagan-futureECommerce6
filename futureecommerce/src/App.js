34afd8788174429aec2faae29b828b7d80fcd654
MacksonWelton <mackson.welton@gmail.com>
2020-02-20 14:35:45 -0300

    Criação de componente produtos

--- futureecommerce/src/App.js
+++ futureecommerce/src/App.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components'
+import Produtos from './Components/Produtos.js'
 
 
 const DivPai = styled.div`
@@ -12,7 +13,7 @@ const Header = styled.div`
 width: 100%;
 height: 100%;
 `
-const Conteudo =styled.div`
+const Conteudo = styled.div`
 display: grid;
 grid-template-columns: 8fr 2fr ;
 width: 100%;
@@ -35,28 +36,41 @@ height: 100%;
 const ProdutosDisplay = styled.div`
 width: 100%;
 height: 100%;
+display: flex;
+flex-wrap: wrap;
 `
 
-class App extends Component  {
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props)
-      this.state={
-          mostraFiltro: false,
-          mostraProdutos: true,
-          mostraCarrinho: false,
-          mostraHeader: true,
-          arrayDeProdutos:[
-            {id: 1,
-            nome: "nave",
-            valorProduto: 100.50,
-            imgUrl:'https://picsum.photos/200/200',
-            },
-            {id: 2,
-              nome: "nave doida",
-              valorProduto: 1212.50,
-              imgUrl:'https://picsum.photos/200/201',
-              }
-          ]
+    this.state = {
+      mostraFiltro: false,
+      mostraProdutos: true,
+      mostraCarrinho: false,
+      mostraHeader: true,
+      arrayDeProdutos: [
+        {
+          id: 1,
+          nome: "nave",
+          valorProduto: 100.50,
+          imgUrl: 'https://picsum.photos/200/200',
+          carrinho: 30
+        },
+        {
+          id: 2,
+          nome: "nave doida",
+          valorProduto: 1212.50,
+          imgUrl: 'https://picsum.photos/200/201',
+          carrinho: 0
+        },
+        {
+          id: 3,
+          nome: "nave doida",
+          valorProduto: 1212.50,
+          imgUrl: 'https://picsum.photos/200/201',
+          carrinho: 0
+        }
+      ]
     }
   }
 
@@ -72,40 +86,61 @@ class App extends Component  {
     })
   }
 
-  render(){
+  adicionarCarrinho = (valorIndex) => {
+    const qtdNoCarrinho = this.state.arrayDeProdutos[valorIndex].carrinho
+    this.setState({
+      arrayDeProdutos 
+    })
+  }
+
+  render() {
     const headerContent = (
       <p>header</p>
-    ) 
+    )
     const filtroContent = (
       <p>filtro</p>
-    ) 
+    )
     const produtosContent = (
-      <p>produtos</p>
-    ) 
+      <div>
+        {this.state.arrayDeProdutos.map(elemento => {
+          return (
+            <Produtos
+              key={this.state.arrayDeProdutos.indexOf(elemento)}
+              id={elemento.id}
+              nome={elemento.nome}
+              valorProduto={elemento.valorProduto}
+              imgUrl={elemento.imgUrl}
+              botaoAdicionaC={this.adicionarCarrinho}
+              indexProduto={this.state.arrayDeProdutos.indexOf(elemento)}
+            />
+          )
+        })}
+      </div>
+    )
     const carrinhoContent = (
       <p>carrinho</p>
-    ) 
+    )
 
     return (
 
       <DivPai>
-         <Header>{this.state.mostraHeader ? headerContent : ''}</Header>
-          <Conteudo>
-           <LateralEsquerda>
-             <Filtro>
-               <button onClick={this.botaoMostraFiltro}>Mostra Filtro</button>
-               {this.state.mostraFiltro ? filtroContent : ''}
-               </Filtro>
-             <ProdutosDisplay>{this.state.mostraProdutos ? produtosContent : ''}</ProdutosDisplay>
-            </LateralEsquerda>
-            <LateralDireita>
-             <button onClick={this.botaoMostraCarrinho}> Mostra Carrinho</button>
-             {this.state.mostraCarrinho ? carrinhoContent : ''}
-            </LateralDireita>
-          </Conteudo>
+        <Header>{this.state.mostraHeader ? headerContent : ''}</Header>
+        <Conteudo>
+          <LateralEsquerda>
+            <Filtro>
+              <button onClick={this.botaoMostraFiltro}>Mostra Filtro</button>
+              {this.state.mostraFiltro ? filtroContent : ''}
+            </Filtro>
+            <ProdutosDisplay>{this.state.mostraProdutos ? produtosContent : ''}</ProdutosDisplay>
+          </LateralEsquerda>
+          <LateralDireita>
+            <button onClick={this.botaoMostraCarrinho}> Mostra Carrinho</button>
+            {this.state.mostraCarrinho ? carrinhoContent : ''}
+          </LateralDireita>
+        </Conteudo>
       </DivPai>
     )
   }
 }
-   
+
 export default App;
--- futureecommerce/src/Components/Produtos.js
+++ futureecommerce/src/Components/Produtos.js
@@ -1,19 +1,40 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components'
 
-class Produtos extends Component  {
-    constructor(props){
-      super(props)
-      
+const ProdutoContainer = styled.div`
+  border: 1pt solid black;
+  display: flex;
+  flex-direction: column;
+`
+const InfoProduto = styled.div`
   
-    }
-    render(){
-      return (
-        <DivPai>
-           
-        </DivPai>
-      )
-    }
+`
+const Imagem = styled.img`
+  max-width: 100%;
+`
+
+const BotaoAdicionar = styled.button``
+
+class Produtos extends Component {
+  constructor(props) {
+    super(props)
+
+
   }
-     
-  export default Produtos;
+  render() {
+    return (
+      <ProdutoContainer>
+        <div>
+          <Imagem src={this.props.imgUrl} />
+        </div>
+        <InfoProduto>
+          <p>{this.props.nome}</p>
+          <p>{this.props.valorProduto}</p>
+          <BotaoAdicionar onClick={() => {this.props.botaoAdicionaC(this.props.indexProduto)}}>Adicionar ao Carrinho</BotaoAdicionar>
+        </InfoProduto>
+      </ProdutoContainer>
+    )
+  }
+}
+
+export default Produtos;
