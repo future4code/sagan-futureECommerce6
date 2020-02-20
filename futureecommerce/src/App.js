ad0071ab3bfa44ca46fe88f8c609dc30544d95de
ThalesMilanezi <milanezitf@gmail.com>
2020-02-20 11:53:51 -0300

    conteudo inicial e função de mostrar e esconder carrinho e filtro

--- futureecommerce/src/App.js
+++ futureecommerce/src/App.js
@@ -3,12 +3,10 @@ import styled from 'styled-components'
 
 
 const DivPai = styled.div`
-
 display: grid;
 grid-template-rows: 1fr 9fr;
 width: 100%;
 height: 100%;
-
 `
 const Header = styled.div`
 width: 100%;
@@ -16,15 +14,13 @@ height: 100%;
 `
 const Conteudo =styled.div`
 display: grid;
-grid-template-columns: 8fr 2fr;
+grid-template-columns: 8fr 2fr ;
 width: 100%;
 height: 100%;
-
 `
 const LateralEsquerda = styled.section`
 display:grid;
-grid-template-rows: 9fr 1fr;
-grid-template-columns: 1fr;
+grid-template-rows: 1fr 9fr;
 width: 100%;
 height: 100%;
 `
@@ -44,24 +40,69 @@ height: 100%;
 class App extends Component  {
   constructor(props){
     super(props)
-    this.state={
-        mensagem: ''
+      this.state={
+          mostraFiltro: false,
+          mostraProdutos: true,
+          mostraCarrinho: false,
+          mostraHeader: true,
+          arrayDeProdutos:[
+            {id: 1,
+            nome: "nave",
+            valorProduto: 100.50,
+            imgUrl:'https://picsum.photos/200/200',
+            },
+            {id: 2,
+              nome: "nave doida",
+              valorProduto: 1212.50,
+              imgUrl:'https://picsum.photos/200/201',
+              }
+          ]
     }
-
-
   }
+
+  botaoMostraFiltro = () => {
+    this.setState({
+      mostraFiltro: !this.state.mostraFiltro
+    })
+  }
+
+  botaoMostraCarrinho = () => {
+    this.setState({
+      mostraCarrinho: !this.state.mostraCarrinho
+    })
+  }
+
   render(){
+    const headerContent = (
+      <p>header</p>
+    ) 
+    const filtroContent = (
+      <p>filtro</p>
+    ) 
+    const produtosContent = (
+      <p>produtos</p>
+    ) 
+    const carrinhoContent = (
+      <p>carrinho</p>
+    ) 
+
     return (
 
       <DivPai>
-         <Header>Header</Header>
-         <Conteudo>Conteudo
-           <LateralEsquerda>lateral esquerda
-             <Filtro>Filtro</Filtro>
-             <ProdutosDisplay>Produtos display</ProdutosDisplay>
-           </LateralEsquerda>
-           <LateralDireita>lateral direita</LateralDireita>
-         </Conteudo>
+         <Header>{this.state.mostraHeader ? headerContent : ''}</Header>
+          <Conteudo>
+           <LateralEsquerda>
+             <Filtro>
+               <button onClick={this.botaoMostraFiltro}>Mostra Filtro</button>
+               {this.state.mostraFiltro ? filtroContent : ''}
+               </Filtro>
+             <ProdutosDisplay>{this.state.mostraProdutos ? produtosContent : ''}</ProdutosDisplay>
+            </LateralEsquerda>
+            <LateralDireita>
+             <button onClick={this.botaoMostraCarrinho}> Mostra Carrinho</button>
+             {this.state.mostraCarrinho ? carrinhoContent : ''}
+            </LateralDireita>
+          </Conteudo>
       </DivPai>
     )
   }
---
+++ futureecommerce/src/Components/Produtos.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import styled from 'styled-components'
+
+class Produtos extends Component  {
+    constructor(props){
+      super(props)
+      
+  
+    }
+    render(){
+      return (
+        <DivPai>
+           
+        </DivPai>
+      )
+    }
+  }
+     
+  export default Produtos;
